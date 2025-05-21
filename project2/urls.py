a5b46dea747d62800742e57a86281db3da30f576
SamratSinghRathore <itssamratsinghrathore888@outlook.com>
2025-05-21 19:53:29 +0200

    Working homepage for project2. Need to add the endpoints for task1

---
+++ project2/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+app_name = "project2"
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('upload/', views.upload_csv, name='upload'),
+    # path('plot/', views.generate_plot, name='plot'),
+    path('train_model/', views.train_model, name='train_model'),
+    path('load_model/', views.load_model, name='load_model')
+]
