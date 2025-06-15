90964fe19fc0426d78beed66118d04692edeaf33
SamratSinghRathore <itssamratsinghrathore888@outlook.com>
2025-06-15 19:38:56 +0200

    resolving merge conflicts and incoming changes fixed

--- project3/urls.py
+++ project3/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from . import views
 
@@ -8,15 +7,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('decision-tree/', views.decision_tree, name='decision_tree'),
     path('logistic-regression/', views.logistic_regression, name='logistic_regression'),
-=======
-from django.urls import path
-from . import views
-
-app_name = 'project3'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('decision-tree/', views.decision_tree, name='decision_tree'),
-    path('logistic-regression/', views.logistic_regression, name='logistic_regression'),
->>>>>>> 23af6a68fd1263c6fef8c68b65a2b9ea76ec0811
 ]
